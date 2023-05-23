bc6f40d9c98ba3d5f247a4bfa0f7bb7f01586b86
Marko Vucinic <marko.vucinic@clarivate.com>
2023-05-23 09:37:30 +0200

    Connect tests and logic for C# Day 1

---
+++ dotnet/src/Day01/Day01.cs
@@ -0,0 +1,34 @@
+namespace Aoc2022;
+
+public class Day01
+{
+    public static int Run(string input)
+    {
+        string[] lines = input.Trim().Split('\n');
+        List<int> calories = new List<int>();
+        int currentCalories = 0;
+
+        foreach (var line in lines)
+        {
+            if (line.Equals(String.Empty))
+            {
+                calories.Add(currentCalories);
+                currentCalories = 0;
+                continue;
+            }
+
+            try
+            {
+                int lineCalories = Int32.Parse(line);
+                currentCalories += lineCalories;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Cannot parse {line} to int32");
+                break;
+            }
+        }
+
+        return calories.Max();
+    }
+}
---
+++ dotnet/src/Day01/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    using (var sr = new StreamReader("./Input"))
+    {
+        string input = sr.ReadToEnd();
+        int result = Aoc2022.Day01.Run(input);
+        Console.WriteLine($"Day 1 result: {result}");
+    }
+}
+catch (IOException)
+{
+    Console.WriteLine("Input file not found.");
+}
+catch (Exception)
+{
+    Console.WriteLine("Something else unplanned happened while trying to read a file");
+}
