2ce6ab4ecbce6364fc3cad7d1bb94c5f88bbe01e
Marko Vucinic <marko.vucinic@clarivate.com>
2023-07-26 21:47:24 +0200

    Solve Day 3 part 2 C#

--- dotnet/src/Day03/Program.cs
+++ dotnet/src/Day03/Program.cs
@@ -1,17 +1,34 @@
-﻿using Aoc2022;
+﻿using System;
+using Aoc2022;
 using Aoc2022.Day03;
 using Extensions;
 
 string[] lines = InputLoader.LoadLines("Input") ?? throw new Exception("Input is empty");
+lines = lines.Where(l => l != "").ToArray();
 
-int result = lines.Sum(line =>
+// Part 1
+int result1 = lines.Sum(line =>
 {
     if (line == string.Empty) return 0;
 
     var (a, b) = line.SplitAtHalf();
-    List<char> found = b.FindOccurences(a);
+    List<char> found = b.FindCommonChars(a);
     return found.Aggregate(0, (acc, c) => acc + CharValue.Get(c));
 });
 
-Console.WriteLine(result);
+// Part 2
+var result2 = 0; 
+var cursor = 0;
+var step = 3;
+
+while (cursor < lines.Length)
+{
+    var group = lines[cursor..(cursor + step)];
+    List<char> found = group[0].FindCommonChars(group[1], group[2]);
+    result2 += found.Aggregate(0, (acc, c) => acc + CharValue.Get(c));
+
+    cursor += step;
+}
+
+Console.WriteLine(result2);
 Console.ReadLine();
--- dotnet/src/Day03/StringExtensions.cs
+++ dotnet/src/Day03/StringExtensions.cs
@@ -1,20 +1,33 @@
-﻿namespace Extensions;
+﻿using System;
+
+namespace Extensions;
 
 public static class StringExtensions
 {
-    public static List<char> FindOccurences(this string haystack, string needle)
+    public static List<char> FindCommonChars(this string s1, string s2)
     {
         List<char> found = new();
 
-        foreach (var c in needle)
+        foreach (var c in s2)
         {
-            if (haystack.Contains(c) && !found.Contains(c))
+            if (s1.Contains(c) && !found.Contains(c))
             {
                 found.Add(c);
             }
         }
 
         return found;
+    }
+
+    public static List<char> FindCommonChars(this string s1, string s2, string s3)
+    {
+        var common1 = s2.FindCommonChars(s1);
+
+        if (common1.Count == 0) return common1;
+
+        var common2 = s3.FindCommonChars(new string(common1.ToArray()));
+
+        return common2;
     }
 
     public static (string, string) SplitAtHalf(this string s)
